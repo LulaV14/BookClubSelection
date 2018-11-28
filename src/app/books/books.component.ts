e31ed231df331aef8a058a0bef5744ea72076ec6
Lula Villalobos <lulavillalobos@gmail.com>
2018-11-28 11:15:37 -0800

    add form filter and actions column to table

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { Book } from '../book';
 import { BOOKS } from '../mock-books';
 
 @Component({
@@ -7,12 +9,22 @@ import { BOOKS } from '../mock-books';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'views'];
-  dataSource = BOOKS;
+  displayedColumns: string[] = ['id', 'name', 'views', 'actions'];
+  dataSource = new MatTableDataSource(BOOKS);
+  selectedBook: Book = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  filterBooks(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  // TODO change to own details page
+  showDetails(book: Book) {
+    this.selectedBook = book;
   }
 
 }
